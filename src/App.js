3ffee2ac556433520e8fd9b09857c5bca575cbdf
Ike <ikescode@gmail.com>
2020-05-07 18:56:26 -0500

    add styles, start fetch and save functions

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  fetch('/random').then(res => {
-    console.log(res);
-  })
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetchPalette = () => {
+  fetch('/random')
+  .then(colors => {
+    this.setState({ colors })
+  });
+}
+
+const savePalette = () => {
+  fetch('/save', {
+    method: "POST", 
+    body: {
+      1: "#NEW",
+      2: "#NEW",
+      3: "#NEW",
+      4: "#NEW",
+      5: "#NEW",
+    }
+  }).then(res => {
+    if (res.status === 200) {
+      alert("Palette saved.")
+    }
+  });
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Color Palette Generator</h1>
+        <div className="paletteWrapper">
+          <div className="color leftRounded"></div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="color"></div>
+          <div className="color rightRounded"></div>
+        </div>
+        <button className="generateButton" onClick={()=> {
+          fetchPalette()
+        }}>Generate</button>
+        <button className="saveButton" onClick={()=> {
+          savePalette()
+        }}>Save</button>
+      </div>
+    );
+  }
 }
 
 export default App;
