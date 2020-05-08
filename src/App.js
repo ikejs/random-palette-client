7c7ae95643ed33d25a034ee9e1ff2ceec8191ddf
Ike <ikescode@gmail.com>
2020-05-07 20:02:51 -0500

    add palette state, finish fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const fetchPalette = () => {
-  fetch('/random')
-  .then(colors => {
-    this.setState({ colors })
-  });
-}
-
 const savePalette = () => {
   fetch('/save', {
     method: "POST", 
@@ -26,19 +19,39 @@ const savePalette = () => {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { 
+      first: `hsl(0, 100%, 50%)`,
+      second: `hsl(80, 100%, 50%)`,
+      third: `hsl(160, 100%, 50%)`,
+      fourth: `hsl(240, 100%, 50%)`,
+      fifth: `hsl(320, 100%, 50%)`
+    }
+  }
+
+  fetchPalette() {
+    fetch('/random')
+    .then(res => res.json())
+    .then(colors => {
+      this.setState(colors)
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Color Palette Generator</h1>
         <div className="paletteWrapper">
-          <div className="color leftRounded"></div>
-          <div className="color"></div>
-          <div className="color"></div>
-          <div className="color"></div>
-          <div className="color rightRounded"></div>
+          <div className="color leftRounded" style={{backgroundColor: this.state.first}}></div>
+          <div className="color" style={{backgroundColor: this.state.second}}></div>
+          <div className="color" style={{backgroundColor: this.state.third}}></div>
+          <div className="color" style={{backgroundColor: this.state.fourth}}></div>
+          <div className="color rightRounded" style={{backgroundColor: this.state.fifth}}></div>
         </div>
         <button className="generateButton" onClick={()=> {
-          fetchPalette()
+          this.fetchPalette()
         }}>Generate</button>
         <button className="saveButton" onClick={()=> {
           savePalette()
