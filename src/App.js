56edd43d0c3ddf353745f6797e54e78e523ed93d
Ike <ikescode@gmail.com>
2020-05-16 02:02:36 -0500

    add loader

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
     super();
 
     this.state = { 
+      loading: false,
       first: `hsl(0, 100%, 50%)`,
       second: `hsl(80, 100%, 50%)`,
       third: `hsl(160, 100%, 50%)`,
@@ -31,12 +32,30 @@ class App extends Component {
     }
   }
 
-  fetchPalette() {
-    fetch('/random')
-    .then(res => res.json())
-    .then(colors => {
-      this.setState(colors)
-    });
+  fetchPalette(hex) {
+    this.setState({ loading: true });
+    console.log((hex ? hex : 'no hex today'));
+    const url = hex ? `/random/${hex}` : '/random';
+      fetch(url)
+      .then(res => res.json())
+      .catch(() => {
+        alert("Waking API. Try again in 5 seconds.");
+      })
+      .then(colors => {
+        this.setState({loading: false, ...colors})
+      });
+  }
+
+  componentDidMount() {
+    this.fetchPalette("#79b452")
+  }
+
+  generateButton() {
+    return (
+      <button className={"generateButton" + (this.state.loading ? "disabled" : "")} onClick={()=> {
+        this.fetchPalette()
+      }}>{(this.state.loading ? "Loading..." : "Generate")}</button>
+    )
   }
 
   render() {
@@ -44,16 +63,26 @@ class App extends Component {
       <div className="App">
         <h1>Color Palette Generator</h1>
         <div className="paletteWrapper">
-          <div className="color leftRounded" style={{backgroundColor: this.state.first.hsl}}></div>
-          <div className="color" style={{backgroundColor: this.state.second.hsl}}></div>
-          <div className="color" style={{backgroundColor: this.state.third.hsl}}></div>
-          <div className="color" style={{backgroundColor: this.state.fourth.hsl}}></div>
-          <div className="color rightRounded" style={{backgroundColor: this.state.fifth.hsl}}></div>
+          <div className="color leftRounded" style={{backgroundColor: this.state.first.hsl}}>
+            <p>{this.state.first.hex}</p>
+          </div>
+          <div className="color" style={{backgroundColor: this.state.second.hsl}}>
+            <p>{this.state.second.hex}</p>
+          </div>
+          <div className="color" style={{backgroundColor: this.state.third.hsl}}>
+            <p>{this.state.third.hex}</p>
+          </div>
+          <div className="color" style={{backgroundColor: this.state.fourth.hsl}}>
+            <p>{this.state.fourth.hex}</p>
+          </div>
+          <div className="color rightRounded" style={{backgroundColor: this.state.fifth.hsl}}>
+            <p>{this.state.fifth.hex}</p>
+          </div>
         </div>
-        <input type="text" className="HEXinput" value={this.state.third.hex}></input>
-        <button className="generateButton" onClick={()=> {
-          this.fetchPalette()
-        }}>Generate</button>
+        <input type="text" className="HEXinput" value={this.state.third.hex} onChange={(e) => {
+          this.fetchPalette(e.target.value)
+        }}></input>
+        {this.generateButton()}
         <button className="saveButton" onClick={()=> {
           savePalette()
         }}>Save</button>
